41a323a63049ee845f80d09af30e68938fca886a
DenisPur <denisudesu@yandex.ru>
2020-04-19 23:23:31 +0300

    first part ready

--- func_mod.f95
+++ func_mod.f95
@@ -2,6 +2,84 @@ module func_mod
 use prec_mod
 contains
 
+function merge(A, B, n) result(AB)
+    implicit none
+    real(mp) :: A(n,n), B(n), AB(0:n, 1:n+1)
+    integer :: n, i
 
+    do i = 1, n+1
+        AB(0, i) = i
+    end do
+
+    AB(1:n, n+1) = B(:)
+    AB(1:n, 1:n) = A(:,:)
+end function
+
+subroutine move_pq(AB, n, p, q, s1, s2)
+    !exchange (p,q) and (s1, s2) 
+    !with their rows and columns
+    implicit none
+    real(mp) :: AB(0:n, 1:n+1), Swap(n+1)
+    integer :: n, p, q, s1, s2
+
+    if(s2 /= q) then
+        Swap(:) = AB(:, s2)
+        AB(:, s2) = AB(:, q)
+        AB(:,  q) = Swap(:)
+    end if
+
+    if(s1 /= p) then
+        Swap(:) = AB(s1, :)
+        AB(s1, :) = AB(p, :)
+        AB( p, :) = Swap
+    end if
+    return
+end subroutine
+
+subroutine make_nonzero(AB, n, s1, s2)
+    implicit none
+    real(mp) :: AB(0:n, 1:n+1), Swap(n+1)
+    integer :: n, p, s1, s2
+
+    if(AB(s1, s2) == 0) then
+        do p = s1+1, n
+            if(AB(p, s2) /= 0) then
+                call move_pq(AB, n, p, s2, s1, s2)  !exch. (p,s2) & (s1, s2) == move rows
+                exit
+            end if
+        end do
+    end if
+    return
+end subroutine
+
+subroutine make_up_triangle(AB, n)
+    implicit none
+    real(mp) :: AB(0:n, 1:n+1)
+    integer :: s, p, n
+
+    do s = 1, n-1
+        call make_nonzero(AB, n, s, s)
+        AB(s, s:n+1) = AB(s, s:n+1) / AB(s,s)
+        do p = s+1, n
+            AB(p, s:n+1) = AB(p, s:n+1) - AB(s, s:n+1) * AB(p,s)
+        end do
+    end do
+    AB(n, n:n+1) = AB(s, n:n+1) / AB(n,n)
+    return
+end subroutine
+
+subroutine make_down_triangle(AB, n)
+    real(mp) :: AB(0:n, 1:n+1)
+    integer :: s, p, n
+
+    do s = n, 2, -1
+        do p = s-1, 1, -1
+            AB(p, n+1) = AB(p, n+1) - AB(s, n+1) * AB(p,s)
+            AB(p, s) = 0
+        end do
+    end do
+    AB(1, n+1) = AB(1, n+1) - AB(1, n+1) * AB(1,1)
+    return
+end subroutine 
 
 end module
--- main.f95
+++ main.f95
@@ -2,13 +2,13 @@ program gauss_linear_system
 use func_mod
 use prec_mod
     implicit none
-    real(mp), allocatable :: A(:,:), B(:), X(:)
+    real(mp), allocatable :: A(:,:), B(:), AB(:,:)
     real(mp) :: resid
     integer :: n, i
 
     n = 3
 
-    allocate(A(n,n), B(n), X(n))
+    allocate(A(n,n), B(n), AB(0:n, 1:n+1))
 
     A(1,:) = (/1, 2, 1/)
     A(2,:) = (/9, 2, 0/)
@@ -16,4 +16,15 @@ use prec_mod
 
     B(:) = (/7, 7, 7/)
 
+    AB = merge(A, B, 3)
+
+    write(*,*) (AB(i, :), new_line('c'), i = 0,n)
+
+    call make_up_triangle(AB, n)
+
+    write(*,*) (AB(i, :), new_line('c'), i = 0,n)
+
+    call make_down_triangle(AB, n)
+
+    write(*,*) (AB(i, :), new_line('c'), i = 0,n)
 end program
