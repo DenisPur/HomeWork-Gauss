dc039f2e8bd3b00291eb58c74ae7c790621a69d9
DenisPur <denisudesu@yandex.ru>
2020-04-19 19:55:57 +0300

    poorly works

--- func_mod.f95
+++ func_mod.f95
@@ -3,6 +3,24 @@ use prec_mod
 contains
 
 
+function solve_wichmax(A, B, n) result(X)
+    implicit none
+    real(mp) :: A(n,n), B(n), AB_Logs(0:n,1:n+1), X(n)
+    integer :: n, i
+
+    do i = 1, n+1
+        AB_Logs(0,i) = i
+    end do
+    AB_Logs(1:n,1:n+1) = merge(A, B, n)
+
+    AB_Logs = up_max_triangle(AB_Logs, n)
+
+    X(:) = 0
+    do i = n, 1, -1 
+        X(i) = AB_Logs(i,n+1) - sum( X(:)*AB_Logs(i,1:n) )
+    end do
+end function
+
 function solve_triangle(A, B, n) result(X)
     implicit none
     real(mp) :: A(n,n), B(n), X(n), AB(n,n+1)
@@ -11,12 +29,10 @@ function solve_triangle(A, B, n) result(X)
     X(:) = 0
     AB = merge(A, B, n)
     AB = up_triangle(AB, n)
-    !write(*,*) (AB(i, :), new_line('c'), i = 1,n)
     do i = n, 1, -1 
         X(i) = AB(i,n+1) - sum( X(:)*AB(i,1:n) )
     end do
 end function
-
 
 function solve_diagonal(A, B, n) result(X)
     implicit none
@@ -31,7 +47,6 @@ function solve_diagonal(A, B, n) result(X)
     end do
 end function
 
-
 function merge(A, B, n) result(AB)
     implicit none
     real(mp) :: A(n,n), B(n), AB(n,n+1)
@@ -43,6 +58,31 @@ function merge(A, B, n) result(AB)
     end do
 end function
 
+recursive function up_max_triangle(AB_Logs, n) result(ABr)
+    implicit none
+    real(mp) :: AB_Logs(0:n,1:n+1), X(1:n+1), Y(0:n)
+    real(mp) :: ABr(0:n,1:n+1), AB_res(0:n-1,1:n), AB_step(0:n-1,1:n)
+    real(mp) :: max
+    integer :: n, i, p, j, q
+
+    AB_Logs(:,:) = make_first_max(AB_Logs, n)
+
+    AB_Logs(1,:) = AB_Logs(1,:) / AB_Logs(1,1)
+    do i = 2, n
+        AB_Logs(i,:) = AB_Logs(i,:) - AB_Logs(1,:)*AB_Logs(i,1)
+    end do
+
+    if(n /= 1) then
+        AB_step(0,     1:n) = AB_Logs(0,   2:n+1)
+        AB_step(1:n-1, 1:n) = AB_Logs(2:n, 2:n+1)
+
+        AB_res = up_max_triangle(AB_step, n-1)
+
+        AB_Logs(0,   2:n+1) = AB_res(0,     1:n)
+        AB_Logs(2:n, 2:n+1) = AB_res(1:n-1, 1:n)
+    end if
+    ABr = AB_Logs
+end function
 
 recursive function up_triangle(AB, n) result(ABr)
     implicit none
@@ -61,7 +101,6 @@ recursive function up_triangle(AB, n) result(ABr)
     end if
     ABr = AB
 end function
-
 
 recursive function down_triangle(AB, n) result(ABr)
     implicit none
@@ -87,7 +126,6 @@ recursive function down_triangle(AB, n) result(ABr)
     ABr = AB
 end function
 
-
 function make_first_nonzero(AB, n) result (ABr)
     implicit none
     real(mp) :: AB(n,n+1), ABr(n,n+1), X(n)
@@ -104,6 +142,32 @@ function make_first_nonzero(AB, n) result (ABr)
     ABr = AB
 end function
 
+function make_first_max(AB_Logs, n) result (ABr)
+    implicit none
+    real(mp) :: AB_Logs(0:n,1:n+1), X(1:n+1), Y(0:n), ABr(0:n,1:n+1)
+    integer :: n, i, j, p, q, max
+
+    max = 0
+    do i = 1, n
+        do j = 1, n
+            if( abs(AB_Logs(i,j)) > max) then
+                max = abs(AB_Logs(i,j))
+                p = i
+                q = j
+            end if
+        end do
+    end do
+
+    Y(0:n) = AB_Logs(:, q)
+    AB_Logs(:, q) = AB_Logs(:, 1)
+    AB_Logs(:, 1) = Y(0:n)
+
+    X(1:n+1) = AB_Logs(p, :)
+    AB_Logs(p, :) = AB_Logs(1, :)
+    AB_Logs(1, :) = X(1:n+1)
+
+    ABr = AB_Logs
+end function
 
 ! function make_last_nonzero(AB, n) result (ABr)
 !     implicit none
@@ -120,6 +184,5 @@ end function
 !     end do
 !     ABr = AB
 ! end function
-
 
 end module
--- main.f95
+++ main.f95
@@ -2,23 +2,43 @@ program gauss_linear_system
 use func_mod
 use prec_mod
     implicit none
-    real(mp), allocatable :: A(:,:), B(:), X(:)
+    real(mp), allocatable :: A(:,:), B(:), X(:), AB_Logs(:,:)
     real(mp) :: resid
-    integer :: n, row
+    integer :: n, i
 
     n = 3
 
-    allocate(A(n,n), B(n), X(n))
+    allocate(A(n,n), B(n), X(n), AB_Logs(0:n,n+1))
 
-    A(1,:) = (/1, 1, 1/)
-    A(2,:) = (/3, 2, 0/)
-    A(3,:) = (/1, 2, 3/)
+    A(1,:) = (/1, 2, 1/)
+    A(2,:) = (/9, 2, 0/)
+    A(3,:) = (/1, 2, 9/)
+    ! A(4,:) = (/4, 3, 2, 2/)
     B(:) = (/7, 7, 7/)
 
     X = solve_triangle(A, B, n)
     write(*,*) X(:)
 
+    write(*,*) '==='
+
     X = solve_diagonal(A, B, n)
     write(*,*) X(:)
+
+    write(*,*) '==='
+
+    X = solve_wichmax(A, B, n)
+    write(*,*) X(:)
+
+    write(*,*) '==='
+
+    do i = 1, n+1
+        AB_Logs(0,i) = i
+    end do
+    AB_Logs(1:n,1:n+1) = merge(A, B, n)
+    write(*,*) (AB_Logs(i, :), new_line('c'), i = 0,n)
+
+    AB_Logs = up_max_triangle(AB_Logs, n)
+    write(*,*) (AB_Logs(i, :), new_line('c'), i = 0,n)
+
 
 end program
